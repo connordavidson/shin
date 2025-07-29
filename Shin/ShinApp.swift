cdd3f6a3095112102ab3ea1ece98ac12f9dfc380
Connor Davidson <connor.j.davidson@gmail.com>
2025-07-29 17:24:51 -0400

    Initial Commit

---
+++ Shin/ShinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinApp.swift
+//  Shin
+//
+//  Created by Connor Davidson on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
